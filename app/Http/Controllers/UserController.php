659901ae318695453fdb1f564986746304f121a0
victron_001 <vicmwe184@gmail.com>
2023-10-18 10:11:06 +0300

    feature - handle errors during user creation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,35 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Models\Farmer;
+use App\Models\StoreManager;
 use App\Models\User;
 use App\Events\CreateUserFarmerEvent;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function create(Request $request)
     {
-        // Create a new user
-        $user = User::create([
-            'first_name' => $request->input('first_name'),
-            'last_name' => $request->input('last_name'),
-            'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password')),
-            'user_type' => $request->input('user_type'),
-        ]);
 
 
-        // Dispatch the event to create a farmer if user_type is 'farmer'
-        if ($request->input('user_type') === 'farmer') {
-            Farmer::create([
-                'user_id' => $user->id,
+        try {
+
+            // Create a new user
+            $user = User::create([
+                'first_name' => $request->input('first_name'),
+                'last_name' => $request->input('last_name'),
+                'email' => $request->input('email'),
+                'password' => bcrypt($request->input('password')),
+                'user_type' => $request->input('user_type'),
             ]);
 
-//            return response()->json(['message' => 'Farmer created successfully'], 201);
-//            echo $user;
+
+            // Dispatch the event to create a farmer if user_type is 'farmer'
+            if ($request->input('user_type') === 'farmer') {
+                Farmer::create([
+                    'user_id' => $user->id,
+                ]);
+
+            }
+
+            if ($request->input('user_type') === 'store_manager'){
+                StoreManager::create([
+                    'user_id' => $user->id,
+                ]);
+            }
+
+            return response()->json(['message' => 'User created successfully'], 201);
+        } catch (QueryException $e) {
+            $errorCode = $e->errorInfo[1];
+
+            if ($errorCode == 1062) { // MySQL unique constraint violation error code
+                return response()->json(['error' => 'This email is already in use.'], 409);
+            }
+
+            // Other types of QueryException errors can be handled here if needed
+
+            return response()->json(['error' => 'Something went wrong.', 'err'=> $e], 500);
         }
 
-        return response()->json(['message' => 'User created successfully'], 201);
+
     }
 
     public function show(){
